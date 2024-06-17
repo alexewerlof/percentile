6eda0ca80c5ebb988e00923540fa6df7bc7afdff
Alex Ewerlöf <hanifbbz@gmail.com>
2024-06-17 09:30:26 +0200

    add a couple more interesting percentiles

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const config = {
-    notablePercentiles: [1, 10, 20, 50, 80, 90, 95, 99, 99.5, 99.9],
+    notablePercentiles: [1, 10, 20, 25, 50, 75, 80, 90, 95, 99, 99.5, 99.9],
     slider: {
         count: 11,
         min: 0,
