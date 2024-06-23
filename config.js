63943ce6f213afc41e2c81ee7d24a3319e77080c
Alex Ewerlöf <hanifbbz@gmail.com>
2024-06-23 14:53:52 +0200

    allow setting frequencies to 0

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export const config = {
     notablePercentiles: [1, 10, 20, 25, 50, 75, 80, 90, 95, 99, 99.5, 99.9],
     slider: {
         count: 20,
-        min: 0.01,
+        min: 0,
         max: 100,
         default: 50,
         step: 0.01,
